18732948c2964f05a927c24617cd14b125f75b2f
albagg3 <garciagonzalezalba@gmail.com>
2024-01-17 18:42:06 +0100

    ex07 finished

--- module_07/ex00/src/main.cpp
+++ module_07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: albagarc <albagarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 17:53:26 by albagarc          #+#    #+#             */
-/*   Updated: 2024/01/17 18:37:59 by albagarc         ###   ########.fr       */
+/*   Updated: 2024/01/17 18:40:47 by albagarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,17 @@ int main ()
 	std::cout << "Function swap: "<< std::endl;
 	std::cout << "one_float is:" << one_float << std::endl;
 	std::cout << "two_float is:" << two_float << std::endl;
+
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 }
