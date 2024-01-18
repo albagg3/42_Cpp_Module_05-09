4c08e446f5ffcdbee7b119d2a51e0e3938af55b0
albagg3 <garciagonzalezalba@gmail.com>
2024-01-18 14:32:42 +0100

    module 07 ex01

--- module_07/ex01/inc/Iter.hpp
+++ module_07/ex01/inc/Iter.hpp
@@ -1,41 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: albagarc <albagarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/17 16:23:38 by albagarc          #+#    #+#             */
-/*   Updated: 2024/01/18 10:45:29 by albagarc         ###   ########.fr       */
+/*   Created: 2024/01/18 10:43:10 by albagarc          #+#    #+#             */
+/*   Updated: 2024/01/18 14:30:51 by albagarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ITER_H
+# define ITER_H
 
-#ifndef WHATEVER_H
-#define WHATEVER_H
+#include <cstdlib>
 
-template < typename T >
-const T&	max(const T& x, const  T& y) 
+template< typename T >
+void	iter(T* address, const size_t arr_length, void(*functionToApply)(T& element))
 {
-	return (y >= x ? y : x);
-}
-
-template < typename T >
-const T&	min(const T& x, const  T& y) 
-{
-	return (y <= x ? y : x);
-}
-
-template < typename T >
-void	swap(T& x, T& y) 
-{
-	T	aux;
-
-	aux = x;
-	x = y;
-	y = aux;
-	return ;
+	for (size_t i = 0; i < arr_length; i++)
+	{
+		functionToApply(address[i]);
+	}
 }
 
 #endif
-// std::template library
--- module_07/ex01/src/main.cpp
+++ module_07/ex01/src/main.cpp
@@ -1,41 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: albagarc <albagarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/17 16:23:38 by albagarc          #+#    #+#             */
-/*   Updated: 2024/01/18 10:45:29 by albagarc         ###   ########.fr       */
+/*   Created: 2024/01/18 11:10:00 by albagarc          #+#    #+#             */
+/*   Updated: 2024/01/18 14:31:21 by albagarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Iter.hpp"
+#include <cstdlib>
+#include <iostream>
 
-#ifndef WHATEVER_H
-#define WHATEVER_H
-
-template < typename T >
-const T&	max(const T& x, const  T& y) 
+template< typename T >
+void functionToApply(T& element)
 {
-	return (y >= x ? y : x);
+	std::cout << element << std::endl;
 }
 
-template < typename T >
-const T&	min(const T& x, const  T& y) 
+
+
+int main ()
 {
-	return (y <= x ? y : x);
+	int arr_int[] = {1, 3, 5, 7};
+	float arr_float[] = {1.2, 3.4, 5.6, 7.8};
+	
+	iter(arr_int, 4, &functionToApply<int>);
+	iter(arr_float, 4, &functionToApply<float>);
+
+    return 0;
 }
-
-template < typename T >
-void	swap(T& x, T& y) 
-{
-	T	aux;
-
-	aux = x;
-	x = y;
-	y = aux;
-	return ;
-}
-
-#endif
-// std::template library
