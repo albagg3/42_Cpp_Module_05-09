e241a6fbf139756c69275d9594bac2d36b467533
albagg3 <garciagonzalezalba@gmail.com>
2024-01-29 17:15:10 +0100

    ex00 done starting ex01

--- module_08/ex00/inc/EasyFind.hpp
+++ module_08/ex00/inc/EasyFind.hpp
@@ -6,19 +6,39 @@
 /*   By: albagarc <albagarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 12:38:35 by albagarc          #+#    #+#             */
-/*   Updated: 2024/01/19 15:41:26 by albagarc         ###   ########.fr       */
+/*   Updated: 2024/01/29 16:52:54 by albagarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_H
 #define EASYFIND_H
 
-< template >< typename T >
-T::iterator idx easyFind(const T& container, int to_find)
+#include <iterator>
+#include <algorithm>
+#include <stdexcept>
+
+template< typename T >
+typename T::iterator easyFind( T& container, int to_find)
 {
-	T::iterator	idx;
-	for()
+	typename T::iterator	it;
+
+	for(it = container.begin(); it != container.end(); ++it)
+	{
+		if(*it == to_find)
+			return it;
+	}
+		throw std::out_of_range("Element not found"); 
 }
 
-
 #endif
+
+// template< typename T >
+// typename T::iterator easyFind( T& container, int to_find)
+// {
+// 	typename T::iterator	it;
+// 	it = std::find(container.begin(), container.end(), to_find);
+// 	if(it != container.end())
+// 		return it;
+// 	else
+// 		throw std::out_of_range("Element not found"); 
+// }
--- module_08/ex00/src/main.cpp
+++ module_08/ex00/src/main.cpp
@@ -6,8 +6,48 @@
 /*   By: albagarc <albagarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 12:38:39 by albagarc          #+#    #+#             */
-/*   Updated: 2024/01/19 12:39:10 by albagarc         ###   ########.fr       */
+/*   Updated: 2024/01/29 16:53:02 by albagarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "EasyFind.hpp"
+#include <iostream>
+#include <list>
+#include <vector>
+
+int main () 
+{
+	//VECTOR
+	try
+	{
+		int array[] = {1, 2 ,3, 4 , 5};
+		std::vector<int> myVector(array, array + sizeof(array) / sizeof(array[0]));
+		std::vector<int>::iterator it;
+		std::vector<int> myvector;
+		std::vector<int>::iterator result;
+
+		result = easyFind(myVector, 7);
+		std::cout << *result << std::endl;
+
+	}
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	//LIST
+	try
+	{
+		int array[] = {1, 2 ,3, 4 , 5};
+		std::list<int> myList(array, array + sizeof(array) / sizeof(array[0]));
+		std::list<int>::iterator iter;
+		iter = easyFind(myList, 3);
+		std::cout << *iter << std::endl;
+	}
+
+	catch(std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+		
+}
