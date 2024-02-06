2e6dfa07884c8173a46d84f90c1f8a47214ceca3
albagg3 <garciagonzalezalba@gmail.com>
2024-02-06 18:14:09 +0100

    ex02 in progress

---
+++ module_08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albagarc <albagarc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 15:19:51 by albagarc          #+#    #+#             */
+/*   Updated: 2024/02/06 18:13:49 by albagarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_H
+# define MUTANTSTACK_H
+
+template < typename T >
+class MutantStack : public std::stack<T>
+{
+	private:
+
+	public:
+	
+};
+
+#endif
