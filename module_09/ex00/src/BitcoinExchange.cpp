c05ba1a5561ed8be678217e81f921d17acc86491
albagg3 <garciagonzalezalba@gmail.com>
2024-02-22 18:33:55 +0100

    valid year month day in progress

--- module_09/ex00/src/BitcoinExchange.cpp
+++ module_09/ex00/src/BitcoinExchange.cpp
@@ -6,11 +6,39 @@
 /*   By: albagarc <albagarc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 12:11:31 by albagarc          #+#    #+#             */
-/*   Updated: 2024/02/15 17:10:47 by albagarc         ###   ########.fr       */
+/*   Updated: 2024/02/22 18:33:30 by albagarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+
+bool	validYearMonthDay(std::string year, std::string month,std::day)
+{
+
+	if (!atoi(year) && !(atoi(year) > 2009))
+		return false;
+	if (!atoi(month) || !(atoi(month) > 0) ||| atoi(month) < 13)
+		return false;
+	if (atoi(day)  && atoi(day) > 0)
+	{
+		if (atoi(month) == 2)
+		{
+			if (atoi(day) < 30)
+				return false
+		}
+		else if (atoi(month) == 1 || atoi(month) == 3 || atoi(month) == 5 || atoi(month) == 7 || atoi(month) == 8 || atoi(month) == 10 || atoi(month) == 12)
+		{
+			if (atoi(day < 32))
+				return false
+		}
+		else
+		{
+			if (atoi(day) < 31)
+				return false
+		}
+	}
+	return true;
+}
 
 BitcoinExchange::BitcoinExchange()
 {
@@ -25,6 +53,7 @@ BitcoinExchange::BitcoinExchange(std::ifstream& inputFile, std::ifstream& dataBa
 	if (!_validDataBase(dataBase))
 		throw BitcoinExchange::InvalidDataBase();
 	// createDataBase()
+
 	std::cout << " BitcoinExchange constructor with files called" << std::endl;
 }
 
@@ -62,10 +91,12 @@ bool	BitcoinExchange::_validDataBase(std::ifstream& dataBase)
 		// this->_validNum("a3");
 		// this->_validNum("t");
 		// this->_validNum("21214132412341341341324132412");
+		this->_validDate("2200-33-42");
+		this->_validDate("2200-1-42");
 	while(std::getline(dataBase,line))
 	{
 		std::cout << line << std::endl;
-		this->_howManyDelimiters(line, ",");
+		// this->_howManyDelimiters(line, ",");
 	// 	if(i % 2 == 0)
 	// 	{
 	// 		if (validDate(line))
@@ -130,17 +161,17 @@ bool BitcoinExchange::_validNum(std::string num)
 
 bool BitcoinExchange::_validDate(std::string date)
 {
-	int hyphenCounter = this->_howManyDelimiters(date, "-")
+	int hyphenCounter = this->_howManyDelimiters(date, "-");
 	if (hyphenCounter != 2 )
 		return false;
 	if (date.length() != 10)
 		return false;
 	
-	std::string year;
-	std::string month;
-	std::string day;
+	if()std::string year = date.substr(0, 4);
+	std::string month = date.substr(5, 2);
+	std::string day = date.substr(8,2);
 	
-
+	std::cout << "year: " << year << " month: " << month << " day: " << day << std::endl;
 	return true;
 
 }
